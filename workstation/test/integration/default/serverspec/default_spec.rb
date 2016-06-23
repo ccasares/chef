98922b7beaffc815ff6a6013bc13e9be2a17c1a8
Carlos Casares <ccasares@gmail.com>
2016-06-23 11:34:28 +0000

    Add tests

--- workstation/test/integration/default/serverspec/default_spec.rb
+++ workstation/test/integration/default/serverspec/default_spec.rb
@@ -3,7 +3,11 @@ require 'spec_helper'
 describe 'workstation::default' do
   # Serverspec examples can be found at
   # http://serverspec.org/resource_types.html
-  it 'does something' do
-    skip 'Replace this with meaningful tests'
+  describe package('tree') do
+      it { should be_installed }
   end
+  describe package('cowsay') do
+      it { should be_installed }
+  end
+
 end
