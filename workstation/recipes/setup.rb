bc982691c56358f5d42c87a158becd15086ed0ad
Carlos Casares <ccasares@gmail.com>
2016-06-23 13:08:22 +0000

    Update default recipes

--- workstation/recipes/setup.rb
+++ workstation/recipes/setup.rb
@@ -6,7 +6,27 @@ package 'tree' do
   action :install
 end
 
+# a pelo
+#file '/etc/motd' do
+#
+#  IPADDRESS = `hostname -I | awk -F' ' '{print $2}'`
+#  HOSTNAME = `hostname`
+#  MEMORY = `cat /proc/meminfo | grep MemTotal | awk -F':' '{print $2}' | sed -e 's/^[ \t]*//'`
+#
+#  content "Property of Casares
+#    IPADDRESS=#{IPADDRESS}
+#    HOSTNAME=#{HOSTNAME}
+#    MEMORY=#{MEMORY}
+#  "
+#end
+
+# con ohai
+
 file '/etc/motd' do
-  content 'blah blah'
+  content "Property of Casares
+    IPADDRESS=#{node['ipaddress']}
+    HOSTNAME=#{node['hostname']}
+    MEMORY=#{node['memory']['total']}
+  "
 end
 
